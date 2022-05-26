a89549637a725c382abd9d3b6a0cdc096f88d1aa
akegiraldo <ake.giraldo@gmail.com>
2022-05-25 23:21:00 -0500

    [task 2] - Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ new authentication mechanism
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -12,4 +13,15 @@ class SessionAuth(Auth):
     Class that inherits from Auth for creating a
     new authentication mechanism
     """
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Function that creates a Session ID for a user_id"""
+        if not user_id or \
+           not isinstance(user_id, str):
+            return None
+
+        session_id = uuid.uuid4().__str__()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
