c19fd23f8ac314a7abd40d3d38c96e42052b7836
akegiraldo <ake.giraldo@gmail.com>
2022-05-25 23:41:22 -0500

    [task 4] - Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,7 @@
 """Class to manage the API authentication"""
 
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -30,3 +31,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Function that validates the current logged user"""
         return None
+
+    def session_cookie(self, request=None):
+        """Funtion that returns a cookie value from a request"""
+        if not request:
+            return None
+
+        cookie_name = getenv("SESSION_NAME")
+        return request.cookies.get(cookie_name)
