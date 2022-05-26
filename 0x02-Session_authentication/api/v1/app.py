a6be4c6e85189df581a5490f56098a130447c560
akegiraldo <ake.giraldo@gmail.com>
2022-05-25 23:15:16 -0500

    [task 1] - Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,6 +16,9 @@ auth = getenv("AUTH_TYPE")
 if auth == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 elif auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Class that inherits from Auth for creating a
+new authentication mechanism
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Class that inherits from Auth for creating a
+    new authentication mechanism
+    """
+    pass
